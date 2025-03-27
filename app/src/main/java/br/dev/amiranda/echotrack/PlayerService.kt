faf485660274b2e71565b22ed1330bf1b6e334fa
Anderson Miranda <anderson.js.mi@outlook.com>
2025-03-26 21:03:42 -0300

    feat: add PlayerService actions and args Handle

--- app/src/main/java/br/dev/amiranda/echotrack/PlayerService.kt
+++ app/src/main/java/br/dev/amiranda/echotrack/PlayerService.kt
@@ -1,98 +1,80 @@
 package br.dev.amiranda.echotrack
 
-import android.Manifest
 import android.annotation.SuppressLint
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
+import android.os.Handler
 import android.os.IBinder
-import android.util.Log
-import androidx.core.app.ActivityCompat
+import android.os.Looper
+import android.os.Message
+import android.os.Messenger
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 
 class PlayerService : Service() {
     companion object {
         private var CHANNEL_ID = "ECHO_PULSE_CHANNEL"
         private var NOTIFICATION_ID = 1
         private var playerRunning = false
-        private var playTrack = false
+
+        private val pcmPlayer = PCMPlayer()
 
         @SuppressLint("StaticFieldLeak")
         private lateinit var notificationBuilder : NotificationCompat.Builder
     }
 
+    private val messenger = Messenger(handler())
+
+    private inner class handler : Handler(Looper.getMainLooper()) {
+
+
+        override fun handleMessage(msg: Message) {
+            val data = msg.data
+            val action = data.getString("action").toString()
+            val args = data.getString("args").toString()
+
+            when(action){
+                "play" -> playFile(args)
+                "stop" -> {
+                    pcmPlayer.stop()
+                    playerRunning = false
+                }
+            }
+
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
-
-        createNotificationChannel()
-        createNotification()
     }
 
     @SuppressLint("ForegroundServiceType")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        createNotification()
-
-        startForeground(NOTIFICATION_ID, notificationBuilder.build())
-
-        notificationBuilder.setContentText("resrd")
-        return START_STICKY
-    }
-
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
-    }
-
-    private fun createNotification() {
         notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Track")
             .setContentText("Artist")
-            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setPriority(NotificationCompat.PRIORITY_MAX)
             .setOngoing(true)
-        /*
-        with(NotificationManagerCompat.from(this)) {
-            // notificationId is a unique int for each notification that you must define.
-            if (ActivityCompat.checkSelfPermission(
-                    this@PlayerService,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
-            notify(NOTIFICATION_ID, notificationBuilder.build())
 
-        }
-        */
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
 
-
+        notificationBuilder.setContentText("EchoTrack Player")
+        return START_STICKY
     }
 
-    private fun createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is not in the Support Library.
-        val channel = NotificationChannel(
-            CHANNEL_ID, "nchannelname",
-            NotificationManager.IMPORTANCE_DEFAULT
-        ).apply {
-            description = "descriptionText"
-        }
-        // Register the channel with the system.
-        val notificationManager: NotificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
-        Log.d("PlayerService", "Notification Channel Created" )
+    override fun onBind(intent: Intent): IBinder {
+        return messenger.binder
     }
-    
+
+    private fun playFile(filepath: String) {
+        if(playerRunning){
+            pcmPlayer.stop()
+        }
+
+        pcmPlayer.setAudioFile(filepath)
+        playerRunning = true
+        pcmPlayer.start()
+
+    }
 }
